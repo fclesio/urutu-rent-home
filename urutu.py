d4431f3705c3a3370d75893ab27c5f8d476a8cf6
flavio.desouza <flavio.silvadesouza@myhammer.net>
2021-01-18 15:46:45 +0100

    Include map

--- urutu.py
+++ urutu.py
@@ -1,7 +1,10 @@
 import sys
 import requests
 import pandas as pd
+import folium
+
 from datetime import datetime
+
 
 extraction_date = datetime.today().strftime("%Y%m%d_%H%M%S")
 
@@ -197,14 +200,103 @@ def save_df_to_disk(df_listings, extraction_date):
     df_listings.to_csv(f"immo24_listings_{extraction_date}.csv", index=False)
 
 
+def generate_map(df_listings_filtered):
+    folium_map = \
+        folium.Map(location=[52.516266, 13.377775],
+                   zoom_start=13,
+                   tiles='OpenStreetMap',
+                   )
+
+    for index, row in df_listings_filtered.iterrows():
+        title = row["title"]
+        price = row["price"]
+        livingSpace = row["livingSpace"]
+        latitude = row["latitude"]
+        longitude = row["longitude"]
+        street = row["street"]
+        quarter = row["quarter"]
+        numberOfRooms = int(row["numberOfRooms"])
+        balcony = row["balcony"]
+        garden = row["garden"]
+        monthlyRate = row["monthlyRate"]
+        builtInKitchen = row["builtInKitchen"]
+        home_url = row["home_url"]
+
+        html = f'''
+        <b>Title</b>: {title}<br>
+        <b>Price</b>: {price} <br>
+        <b>LivingSpace</b>: {livingSpace} <br>
+        <b>Street</b>: {street} <br>
+        <b>Quarter</b>: {quarter} <br>
+        <b>NumberOfRooms</b>: {numberOfRooms} <br>
+        <b>Balcony</b>: {balcony} <br>
+        <b>Garden</b>: {garden} <br>
+        <b>MonthlyRate</b>: {monthlyRate} <br>
+        <b>BuiltInKitchen</b>: {builtInKitchen} <br>
+        <b>home_url</b>: <a href="{home_url}">{home_url}</a> <br>
+        '''
+
+        iframe = folium.IFrame(html,
+                               width=450,
+                               height=250)
+
+        popup = folium.Popup(iframe,
+                             max_width=450)
+
+        if numberOfRooms == 1:
+            color = "#cc3139"  # Red
+        elif numberOfRooms == 2:
+            color = "#39cc31"  # Green
+        elif numberOfRooms >= 3:
+            color = "#3139cc"  # Blue
+        else:
+            color = "#cc3139"
+
+        folium.CircleMarker(
+            location=[latitude, longitude],
+            radius=3,
+            popup=popup,
+            color=color,
+            fill=True).add_to(folium_map)
+
+    folium_map.save("berlin_housing_map.html")
+
+
 def main():
     sys.stdout.write(str("Execution started...") + '\n')
+
+    sys.stdout.write(str("Search pages...") + '\n')
     searchresults = get_immo24_search_pages()
+
+    sys.stdout.write(str("Processing listings results...") + '\n')
     listing_results = get_listings_detailed_info(searchresults)
     df_listings_with_columns = get_df_with_columns(listing_results)
     df_listings = sort_df_listings(df_listings_with_columns)
+
+    sys.stdout.write(str("Save results in csv...") + '\n')
     save_df_to_disk(df_listings, extraction_date)
-    sys.stdout.write(str("Execution fininshed...") + '\n')
+
+    sys.stdout.write(str("Generate map with location data...") + '\n')
+    df_listings_filtered_latitude \
+        = df_listings_with_columns[df_listings_with_columns['latitude'] != 'N/A']
+
+    df_listings_filtered_longitude \
+        = df_listings_filtered_latitude[df_listings_filtered_latitude['longitude'] != 'N/A']
+
+    df_listings_filtered = df_listings_filtered_longitude
+
+    generate_map(df_listings_filtered)
+    sys.stdout.write(str("Map generated...") + '\n')
+
+    total_listings = df_listings.shape[0]
+    total_listings_in_map = df_listings_filtered.shape[0]
+    listings_offset = total_listings - total_listings_in_map
+
+    sys.stdout.write(str("Stats ") + '\n')
+    sys.stdout.write(str(f"Total Listings: {total_listings}") + '\n')
+    sys.stdout.write(str(f"Total Listings in Map: {total_listings_in_map}") + '\n')
+    sys.stdout.write(str(f"Total Listings not in the Map: {listings_offset}") + '\n')
+    sys.stdout.write(str("Execution finished...") + '\n')
 
 
 if __name__ == "__main__":
